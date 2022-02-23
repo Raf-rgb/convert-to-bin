568e7624d71ae4c1937eefa515e3964cc3ee1fa2
Rafael <59372766+Raf-rgb@users.noreply.github.com>
2022-02-22 22:31:32 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/getBin/<string:text>')
+def get_bin(text):
+    # Se reempla el _ por un espacio
+    text = text.replace("_", " ")
+    bin = ""
+
+    for s in text:
+        bin += " " + decimal_to_bin(ord(s))
+
+    return jsonify({ "binario" : bin})
+
+# Funcion para convertir un decimal a binario
+def decimal_to_bin(decimal):
+    bin = ""
+
+    if decimal > 0:
+        while decimal > 0:
+            if decimal % 2 == 0: bin += "0"
+            else: bin += "1"
+
+            decimal = int(decimal / 2)
+    else:
+        bin = "0"
+        
+    return "0" + bin[::-1]
+
+if __name__ == '__main__':
+    app.run()
